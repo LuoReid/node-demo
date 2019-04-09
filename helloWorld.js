d825470db43af1241dd2659da66a08501af81752
Luo Reid <luo.reid@gmail.com>
2019-04-09 23:42:05 +0800

    router example

--- helloWorld.js
+++ helloWorld.js
@@ -1,7 +1,22 @@
 var http = require('http')
 
-http.createServer(function(req,res){
-  res.writeHead(200,{'Content-Type':'text/plain'})
-  res.end('Hello World:)')
+http.createServer(function (req, res) {
+  var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
+  switch (path) {
+    case '':
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.end('home page:)')
+      break;
+    case '/about':
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.end('About:)')
+      break;
+    default:
+      res.writeHead(404, { 'Content-Type': 'text/plain' })
+      res.end('Not Found:(')
+      break;
+  }
+  // res.writeHead(200,{'Content-Type':'text/plain'})
+  // res.end('Hello World:)')
 }).listen(3000)
 console.log('Server started on localhost:3000; press Ctrl-C to terminate...')
